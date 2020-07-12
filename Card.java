85316178f34b864354d150b9db72a556f2bb19cb
Jeff Haupt <66330433+jeff1haupt@users.noreply.github.com>
2020-07-12 17:47:38 -0400

    Updated Card and Deck Classes

--- Card.java
+++ Card.java
@@ -1,5 +1,29 @@
 package week6;
 
 public class Card {
+	
+	private int value;
+	private String name;
+	
+	public Card(String cardName, int cardValue) {
+		// TODO Auto-generated constructor stub
+	}
 
+	public String describe() {
+		return Card.class.getSimpleName() + "[name=" + name + ", value=" + value + "]";
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
--- Deck.java
+++ Deck.java
@@ -1,5 +1,37 @@
 package week6;
 
+import java.util.List;
+
+
 public class Deck {
 
+	List<Card> cards;
+	
+	private static final String[] suits = {"Clubs", "Hearts", "Spades", "Diamonds"};
+	
+	private static final String[] names = {"Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Jack", "Queen", "King", "Ace"};
+	
+	public Deck() {
+		for(String suit : suits) {
+			for(int i = 0; i < names.length; i++) {
+				String name = names[i];
+				String cardName = name + " of " + suit;
+				int cardValue = i + 2;
+				
+				Card card = new Card(cardName, cardValue);
+			}
+		}
+	}
+	
+	
+	public void shuffle() {
+		
+		
+	}
+	
+	public Card draw() {
+		return null;
+		
+	}
+	
 }
